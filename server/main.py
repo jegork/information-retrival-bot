1d11ead1301504470d51d7eabdb00fac4d5d6317
Jegor Kitskerkin <jegor.kitskerkin@gmail.com>
2021-11-04 16:13:21 +0100

    Support for multiple user, remove deprecated code
    - Instead of using single Manager and socketio client, create a Connection class that handles the overall connection.
    - Remove deprecated code that used HTTP in FastAPI
    - Small refactoring main.py
    - Make requirements.txt more explicit (in crucial dependencies)

--- server/main.py
+++ server/main.py
@@ -1,6 +1,8 @@
-import json
+#from fastapi.logger import logger
 import os
-import httpx
+from typing import List
+import logging
+
 import requests
 import socketio
 from bs4 import BeautifulSoup
@@ -10,31 +12,78 @@ from mtranslate import translate
 from pydantic import BaseModel
 from starlette.responses import FileResponse
 
+RASA_URL = f'http://{os.getenv("RASA_HOST", "localhost")}:5005/'
+
+logger = logging.getLogger(__name__)
+
 
 class Message(BaseModel):
     text: str
     sender: str
 
 
-class Manager():
-    def __init__(self):
-        self.websocket = None
-        self.lang = None
-        self.user_id = None
-
-    def set_websocket(self, websocket: WebSocket):
+class Connection:
+    def __init__(self, websocket: WebSocket, socketio: socketio.AsyncClient):
         self.websocket = websocket
+        self.socketio = socketio
+        self.lang = 'en'
 
-    def set_lang(self, lang: str):
-        self.lang = lang
+        self.socketio.on('bot_uttered', self.bot_uttered)
 
-    def set_user_id(self, user_id: int):
-        self.user_id = user_id
+    async def connect(self) -> bool:
+        await self.websocket.accept()
+        await self.websocket.send_json({'status': 'Connected'})
+
+        try:
+            await self.socketio.connect(RASA_URL)
+        except socketio.exceptions.ConnectionError:
+            await self.websocket.close()
+            return False
+
+        return True
+
+    async def bot_uttered(self, data):
+        response_data = {}
+
+        if 'text' in data:
+            if self.lang != 'en':
+                text = translate(data['text'], self.lang, 'en')
+            else:
+                text = data['text']
+
+            response_data['text'] = text
+
+        if 'link' in data:
+            r = requests.get(data['link'])
+
+            soup = BeautifulSoup(r.text, features='html.parser')
+            ad_viewport = soup.find(class_='carrousel__viewport')
+
+            if ad_viewport is not None:
+                response_data['image'] = ad_viewport.find(class_='picture__image')['src']
+
+        await self.websocket.send_json(response_data)
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.connections: List[Connection] = []
+
+    async def add_connection(self, websocket: WebSocket, socketio: socketio.AsyncClient) -> int:
+        new_connection = Connection(websocket=websocket, socketio=socketio)
+        self.connections.append(new_connection)
+
+        return len(self.connections) - 1
+
+    def set_language(self, connection_id: int, lang: str):
+        self.connections[connection_id].lang = lang
+
+    async def connect(self, connection_id: int) -> bool:
+        return await self.connections[connection_id].connect()
 
 
 app = FastAPI()
-manager = Manager()
-sio = socketio.AsyncClient()
+connection_manager = ConnectionManager()
 
 app.add_middleware(
     CORSMiddleware,
@@ -51,75 +100,25 @@ def main_page():
 
 
 @app.websocket('/ws')
-async def websocket_endpoint(websocket: WebSocket):
-    manager.set_websocket(websocket)
-    await sio.connect(f'http://{os.getenv("RASA_HOST", "localhost")}:5005/')
+async def websocket(websocket: WebSocket):
+    connection_id = await connection_manager.add_connection(websocket, socketio.AsyncClient())
+    connected = await connection_manager.connect(connection_id)
 
-    await websocket.accept()
+    if connected:
+        try:
+            while True:
+                data = await connection_manager.connections[connection_id].websocket.receive_json()
+                connection_manager.set_language(connection_id, data['lang'])
 
-    try:
-        while True:
-            data = await websocket.receive_json()
-            manager.set_lang(data['lang'])
-            if data['lang'] != 'en':
-                text = translate(data['message'], 'en', data['lang'])
-            else:
-                text = data['message']
+                if data['lang'] != 'en':
+                    text = translate(data['message'], 'en', data['lang'])
+                else:
+                    text = data['message']
 
-            await sio.emit("user_uttered", data={'message': text})
-    except WebSocketDisconnect:
-        await sio.disconnect()
-        print('User disconnected')
+                await connection_manager.connections[connection_id].socketio.emit("user_uttered",
+                                                                                  data={'message': text})
 
-
-async def request(sender: str, msg: str):
-    async with httpx.AsyncClient() as client:
-        response = await client.post(f'http://{os.getenv("RASA_HOST", "localhost")}:5005/webhooks/rest/webhook',
-                                     json={'sender': sender, 'message': msg})
-
-    return json.loads(response.text)[0]
-
-
-@app.post('/chat/{lang}')
-async def chat(lang: str, message: Message):
-    if lang != 'en':
-        text = translate(message.text, 'en', lang)
-
-        response = await request(message.sender, text)
-
-        return {'text': translate(response['text'], lang, 'en')}
+        except WebSocketDisconnect:
+            await connection_manager.connections[connection_id].socketio.disconnect()            
     else:
-        text = message.text
-
-        response = await request(message.sender, text)
-
-        return {'text': response['text']}
-
-
-@sio.on('connect')
-def connect():
-    print('Rasa socket.io connected')
-
-
-@sio.on('bot_uttered')
-async def bot_uttered(msg):
-    response_data = {}
-
-    if 'text' in msg:
-        if manager.lang != 'en':
-            text = translate(msg['text'], manager.lang, 'en')
-        else:
-            text = msg['text']
-
-        response_data['text'] = text
-
-    if 'attachment' in msg:
-        r = requests.get(msg['attachment'])
-
-        soup = BeautifulSoup(r.text, features='html.parser')
-        ad_viewport = soup.find(class_='carrousel__viewport')
-
-        if ad_viewport is not None:
-            response_data['image'] = ad_viewport.find(class_='picture__image')['src']
-
-    await manager.websocket.send_json(response_data)
+        logger.error('Failed to establish connection!')
